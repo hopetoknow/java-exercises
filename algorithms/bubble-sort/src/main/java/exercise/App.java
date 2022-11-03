f17536b3251ccb635b5f27729233364d47fddb27
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2022-11-03 22:44:36 +0100

    Add algorithm and tests

---
+++ algorithms/bubble-sort/src/main/java/exercise/App.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class App {
+
+    public static void bubbleSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
