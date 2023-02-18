ee7bf82a36c5d67b23b50e362e6aade5963c5515
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-02-18 19:46:43 +0100

    Add solution

---
+++ core/fizz-buzz/src/main/java/exercise/App.java
@@ -0,0 +1,18 @@
+package exercise;
+
+public class App {
+
+    public static void fizzBuzz(int begin, int end) {
+        for (int i = begin; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- core/power-of-three/src/main/java/exercise/App.java
+++ core/power-of-three/src/main/java/exercise/App.java
@@ -2,7 +2,7 @@ package exercise;
 
 public class App {
 
-    public static Boolean isPowerOfThree(int number) {
+    public static boolean isPowerOfThree(int number) {
         if (number <= 0) {
             return false;
         }
