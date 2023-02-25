bab8eb97af97eb225027854c84129af0d1adb8e9
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-02-25 12:38:19 +0100

    Add second solution

--- core/add-digits/src/main/java/exercise/App.java
+++ core/add-digits/src/main/java/exercise/App.java
@@ -16,4 +16,31 @@ public class App {
 
         return result;
     }
+
+    public static int addDigits2(int number) {
+        int result = number;
+
+        while (!isOneDigitNumber(result)) {
+            result = sumDigits(result);
+        }
+
+        return result;
+    }
+
+    private static int sumDigits(int number) {
+        String numberAsString = Integer.toString(number);
+        int result = 0;
+
+        for (int i = 0; i < numberAsString.length(); i++) {
+            String digitAsString = Character.toString(numberAsString.charAt(i));
+            int digit = Integer.parseInt(digitAsString);
+            result += digit;
+        }
+
+        return result;
+    }
+
+    private static boolean isOneDigitNumber(int number) {
+        return number < 10;
+    }
 }
--- core/add-digits/src/test/java/exercise/AppTest.java
+++ core/add-digits/src/test/java/exercise/AppTest.java
@@ -16,4 +16,15 @@ class AppTest {
         assertThat(App.addDigits(38)).isEqualTo(2);
         assertThat(App.addDigits(1259)).isEqualTo(8);
     }
+
+    @Test
+    public void testAddDigits2() {
+        assertThat(App.addDigits2(0)).isEqualTo(0);
+        assertThat(App.addDigits2(1)).isEqualTo(1);
+        assertThat(App.addDigits2(9)).isEqualTo(9);
+        assertThat(App.addDigits2(10)).isEqualTo(1);
+        assertThat(App.addDigits2(19)).isEqualTo(1);
+        assertThat(App.addDigits2(38)).isEqualTo(2);
+        assertThat(App.addDigits2(1259)).isEqualTo(8);
+    }
 }
