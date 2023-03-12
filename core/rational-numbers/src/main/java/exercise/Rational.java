72d8669c81e28b1b95ee8b802c4e3390afdfa327
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-03-12 17:35:09 +0100

    Add solution

---
+++ core/rational-numbers/src/main/java/exercise/Rational.java
@@ -0,0 +1,45 @@
+package exercise;
+
+class Rational {
+
+    private final int numerator;
+    private final int denominator;
+
+    Rational(int numerator, int denominator) {
+        int gcd = gcd(numerator, denominator);
+        this.numerator = numerator / gcd;
+        this.denominator = denominator / gcd;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public Rational add(Rational rational) {
+        return new Rational(
+                numerator * rational.getDenominator() + rational.getNumerator() * denominator,
+                denominator * rational.getDenominator());
+    }
+
+    public Rational sub(Rational rational) {
+        return new Rational(
+                numerator * rational.getDenominator() - rational.getNumerator() * denominator,
+                denominator * rational.getDenominator());
+    }
+
+    public String toString() {
+        return String.format("%d/%d", numerator, denominator);
+    }
+
+    private int gcd(int a, int b) {
+        if (b==0) {
+            return a;
+        }
+
+        return gcd(Math.abs(b), a % b);
+    }
+}
