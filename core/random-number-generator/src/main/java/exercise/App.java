97ad1644d11bc8597bcd79e44c297b6a917cb574
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-03-15 19:06:35 +0100

    Add solution

--- core/random-number-generator/src/main/java/exercise/App.java
+++ core/random-number-generator/src/main/java/exercise/App.java
@@ -2,4 +2,20 @@ package exercise;
 
 class Random {
 
+    private int seed;
+    private final int initSeed;
+
+    Random(int seed) {
+        this.seed = seed;
+        this.initSeed = seed;
+    }
+
+    public int getNext() {
+        seed = (int) ((seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1));
+        return seed;
+    }
+
+    void reset() {
+        seed = initSeed;
+    }
 }
