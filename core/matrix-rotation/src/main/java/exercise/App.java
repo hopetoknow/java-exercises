d49ce18962d62ffc22b8f09e248726912943951f
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-03-25 14:45:47 +0100

    Add solution

---
+++ core/matrix-rotation/src/main/java/exercise/App.java
@@ -0,0 +1,30 @@
+package exercise;
+
+class App {
+
+    public static int[][] rotateLeft(int[][] matrix) {
+        return rotate(matrix, "left");
+    }
+
+    public static int[][] rotateRight(int[][] matrix) {
+        return rotate(matrix, "right");
+    }
+
+    private static int[][] rotate(int[][] matrix, String direction) {
+        int rowNumber = matrix.length;
+        int columnNumber = matrix[0].length;
+        int[][] result = new int[columnNumber][matrix.length];
+
+        for (int i = 0; i < columnNumber; i++) {
+            for (int j = 0; j < rowNumber; j++) {
+                if ("left".equals(direction)) {
+                    result[i][j] = matrix[j][columnNumber - i - 1];
+                } else {
+                    result[i][j] = matrix[rowNumber - j - 1][i];
+                }
+            }
+        }
+
+        return result;
+    }
+}
