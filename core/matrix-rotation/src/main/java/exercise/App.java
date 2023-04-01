a3c61017a8f8939aa9ac5f328b8e2b9e985a4d29
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-01 17:31:44 +0200

    Fix syntax

--- core/matrix-rotation/src/main/java/exercise/App.java
+++ core/matrix-rotation/src/main/java/exercise/App.java
@@ -13,7 +13,7 @@ class App {
     private static int[][] rotate(int[][] matrix, String direction) {
         int rowNumber = matrix.length;
         int columnNumber = matrix[0].length;
-        int[][] result = new int[columnNumber][matrix.length];
+        int[][] result = new int[columnNumber][rowNumber];
 
         for (int i = 0; i < columnNumber; i++) {
             for (int j = 0; j < rowNumber; j++) {
