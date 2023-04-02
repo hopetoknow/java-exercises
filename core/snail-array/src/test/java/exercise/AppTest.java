c7ff7936ff1003b89c57706e6329dd83cc34bbdb
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-02 18:28:29 +0200

    Add more tests

--- core/snail-array/src/test/java/exercise/AppTest.java
+++ core/snail-array/src/test/java/exercise/AppTest.java
@@ -9,12 +9,47 @@ class AppTest {
     @Test
     public void testBuildSnailPath1() {
         int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+        int[] expected = {1, 2, 4, 3};
+        int[] actual = App.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath2() {
+        int[][] matrix = {
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}
         };
         int[] expected = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
-        var actual = App.buildSnailPath(matrix);
+        int[] actual = App.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath3() {
+        int[][] matrix = {};
+        int[] expected = {};
+        int[] actual = App.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath4() {
+        int[][] matrix = {{1, 2, 3, 4}};
+        int[] expected = {1, 2, 3, 4};
+        int[] actual = App.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath5() {
+        int[][] matrix = {{1}, {2}, {3}, {4}};
+        int[] expected = {1, 2, 3, 4};
+        int[] actual = App.buildSnailPath(matrix);
         assertThat(actual).containsExactly(expected);
     }
 }
