bb45b68bfee1e7080ea89da5f1fed60b05acddf6
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-06 22:27:23 +0200

    Add more tests

--- core/battleship/src/test/java/exercise/AppTest.java
+++ core/battleship/src/test/java/exercise/AppTest.java
@@ -7,6 +7,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AppTest {
 
     @Test
+    public void testCalcShipsCount() {
+        int[][] battleField1 = {};
+        assertThat(App.calcShipsCount(battleField1)).isEqualTo(0);
+    }
+
+    @Test
+    public void testCalcShipsCount2() {
+        int[][] battleField2 = {{1}};
+        assertThat(App.calcShipsCount(battleField2)).isEqualTo(1);
+    }
+
+    @Test
+    public void testCalcShipsCount3() {
+        int[][] battleField3 = {{0}};
+        assertThat(App.calcShipsCount(battleField3)).isEqualTo(0);
+    }
+
+    @Test
+    public void testCalcShipsCount4() {
+        int[][] battleField4 = {
+                {0, 0, 1},
+                {0, 0, 0},
+                {1, 1, 0},
+        };
+        assertThat(App.calcShipsCount(battleField4)).isEqualTo(2);
+    }
+
+    @Test
+    public void testCalcShipsCount5() {
+        int[][] battleField5 = {
+                {1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0, 1},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 1, 0, 0},
+        };
+        assertThat(App.calcShipsCount(battleField5)).isEqualTo(6);
+    }
+
+    @Test
     public void testValidateField() {
         int[][] battleField1 = {};
         assertThat(App.isValidField(battleField1)).isTrue();
