5769d64d2621476b0b2cedade607df146908d069
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-19 19:19:30 +0200

    Add tests for second solution of nrzi-encoding

--- exercises/collections/nrzi-encoding/src/test/java/exercise/AppTest.java
+++ exercises/collections/nrzi-encoding/src/test/java/exercise/AppTest.java
@@ -3,6 +3,7 @@ package exercise;
 import org.junit.jupiter.api.Test;
 
 import static exercise.App.nrzi;
+import static exercise.App.nrzi2;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AppTest {
@@ -31,6 +32,33 @@ class AppTest {
         assertThat(actual3).isEqualTo("010010000100111");
 
         String actual4 = nrzi("|¯|___|¯¯¯¯¯|___|¯|_|¯");
+        assertThat(actual4).isEqualTo("110010000100111");
+    }
+
+    @Test
+    void testNrziWhenEmptySignal2() {
+        String actual = nrzi2("");
+        assertThat(actual).isEqualTo("");
+    }
+
+    @Test
+    void testNrziWhenInvalidSignal2() {
+        String actual = nrzi2("|");
+        assertThat(actual).isEqualTo("");
+    }
+
+    @Test
+    void testNrzi2() {
+        String actual1 = nrzi2("¯|__|¯|___|¯¯");
+        assertThat(actual1).isEqualTo("010110010");
+
+        String actual2 = nrzi2("_|¯¯¯|_|¯¯¯¯|_|¯¯");
+        assertThat(actual2).isEqualTo("010011000110");
+
+        String actual3 = nrzi2("¯|___|¯¯¯¯¯|___|¯|_|¯");
+        assertThat(actual3).isEqualTo("010010000100111");
+
+        String actual4 = nrzi2("|¯|___|¯¯¯¯¯|___|¯|_|¯");
         assertThat(actual4).isEqualTo("110010000100111");
     }
 }
