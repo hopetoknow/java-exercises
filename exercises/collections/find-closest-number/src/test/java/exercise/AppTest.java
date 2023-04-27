7a5938354b63768a8a378c69f884745c94819028
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-27 22:28:23 +0200

    Add tests for second solution to find-closest-number

--- exercises/collections/find-closest-number/src/test/java/exercise/AppTest.java
+++ exercises/collections/find-closest-number/src/test/java/exercise/AppTest.java
@@ -43,4 +43,39 @@ class AppTest {
         int actual8 = App.findIndexOfClosest(numbers8, 4);
         assertThat(actual8).isEqualTo(2);
     }
+
+    @Test
+    void testFindIndexOfClosest2() {
+        List<Integer> numbers1 = new ArrayList<>();
+        int actual1 = App.findIndexOfClosest(numbers1, 2);
+        assertThat(actual1).isEqualTo(-1);
+
+        List<Integer> numbers2 = List.of(10);
+        int actual2 = App.findIndexOfClosest(numbers2, 0);
+        assertThat(actual2).isEqualTo(0);
+
+        List<Integer> numbers3 = List.of(10, 15);
+        int actual3 = App.findIndexOfClosest(numbers3, 0);
+        assertThat(actual3).isEqualTo(0);
+
+        List<Integer> numbers4 = List.of(15, 10);
+        int actual4 = App.findIndexOfClosest(numbers4, 0);
+        assertThat(actual4).isEqualTo(1);
+
+        List<Integer> numbers5 = List.of(15, 10);
+        int actual5 = App.findIndexOfClosest(numbers5, 12);
+        assertThat(actual5).isEqualTo(1);
+
+        List<Integer> numbers6 = List.of(15, 10, 3, 4);
+        int actual6 = App.findIndexOfClosest(numbers6, 0);
+        assertThat(actual6).isEqualTo(2);
+
+        List<Integer> numbers7 = List.of(7, 5, 3, 2);
+        int actual7 = App.findIndexOfClosest(numbers7, 4);
+        assertThat(actual7).isEqualTo(1);
+
+        List<Integer> numbers8 = List.of(7, 5, 4, 4, 3, 6);
+        int actual8 = App.findIndexOfClosest(numbers8, 4);
+        assertThat(actual8).isEqualTo(2);
+    }
 }
