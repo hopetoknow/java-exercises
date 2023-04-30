fcc2a0f224a0dcafbed1d579854a2544ef0bca2b
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-04-30 22:15:35 +0200

    Add more tests for calculateProbabilities() method

--- exercises/collections/probability-theory/src/test/java/exercise/AppTest.java
+++ exercises/collections/probability-theory/src/test/java/exercise/AppTest.java
@@ -2,12 +2,20 @@ package exercise;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AppTest {
+
+    @Test
+    void testProbabilitiesEmpty() {
+        assertThat(App.calculateProbabilities(new ArrayList<Integer>()))
+                .isEqualTo(new HashMap<Integer, Map<Integer, Double>>());
+    }
 
     @Test
     void testProbabilities1() {
@@ -18,8 +26,7 @@ class AppTest {
                 5, Map.of(1, 1.0)
         );
 
-        assertThat(App.calculateProbabilities(numbers))
-                .isEqualTo(expected);
+        assertThat(App.calculateProbabilities(numbers)).isEqualTo(expected);
     }
 
     @Test
@@ -33,7 +40,33 @@ class AppTest {
                 6, Map.of()
         );
 
-        assertThat(App.calculateProbabilities(numbers))
-                .isEqualTo(expected);
+        assertThat(App.calculateProbabilities(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testProbabilities3() {
+        List<Integer> numbers = List.of(2, 3, 2, 5, 2, 2, 1, 3);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(3, 1.0),
+                2, Map.of(1, 0.25, 2, 0.25, 3, 0.25, 5, 0.25),
+                3, Map.of(2, 1.0),
+                5, Map.of(2, 1.0)
+        );
+
+        assertThat(App.calculateProbabilities(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testProbabilities4() {
+        List<Integer> numbers = List.of(1, 3, 1, 5, 1, 2, 1, 6, 1, 3);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(2, 0.2, 3, 0.4, 5, 0.2, 6, 0.2),
+                2, Map.of(1, 1.0),
+                3, Map.of(1, 1.0),
+                5, Map.of(1, 1.0),
+                6, Map.of(1, 1.0)
+        );
+
+        assertThat(App.calculateProbabilities(numbers)).isEqualTo(expected);
     }
 }
