29aea5a7c2386d30e2b144d12d2b6c780c142473
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-05-19 19:02:04 +0200

    Add tests for second solution to probability-theory exercise

--- exercises/collections/probability-theory/src/test/java/exercise/AppTest.java
+++ exercises/collections/probability-theory/src/test/java/exercise/AppTest.java
@@ -12,13 +12,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class AppTest {
 
     @Test
-    void testProbabilitiesEmpty() {
-        assertThat(App.calculateProbabilities(new ArrayList<Integer>()))
+    void testCalculateProbabilitiesEmpty() {
+        assertThat(App.calculateProbabilities(new ArrayList<>()))
                 .isEqualTo(new HashMap<Integer, Map<Integer, Double>>());
     }
 
     @Test
-    void testProbabilities1() {
+    void testCalculateProbabilities1() {
         List<Integer> numbers = List.of(1, 3, 1, 5, 1);
         Map<Integer, Map<Integer, Double>> expected = Map.of(
                 1, Map.of(3, 0.5, 5, 0.5),
@@ -30,7 +30,7 @@ class AppTest {
     }
 
     @Test
-    void testProbabilities2() {
+    void testCalculateProbabilities2() {
         List<Integer> numbers = List.of(1, 3, 1, 5, 1, 2, 1, 6);
         Map<Integer, Map<Integer, Double>> expected = Map.of(
                 1, Map.of(2, 0.25, 3, 0.25, 5, 0.25, 6, 0.25),
@@ -44,7 +44,7 @@ class AppTest {
     }
 
     @Test
-    void testProbabilities3() {
+    void testCalculateProbabilities3() {
         List<Integer> numbers = List.of(2, 3, 2, 5, 2, 2, 1, 3);
         Map<Integer, Map<Integer, Double>> expected = Map.of(
                 1, Map.of(3, 1.0),
@@ -57,7 +57,7 @@ class AppTest {
     }
 
     @Test
-    void testProbabilities4() {
+    void testCalculateProbabilities4() {
         List<Integer> numbers = List.of(1, 3, 1, 5, 1, 2, 1, 6, 1, 3);
         Map<Integer, Map<Integer, Double>> expected = Map.of(
                 1, Map.of(2, 0.2, 3, 0.4, 5, 0.2, 6, 0.2),
@@ -68,5 +68,64 @@ class AppTest {
         );
 
         assertThat(App.calculateProbabilities(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testCalculateProbabilities2Empty() {
+        assertThat(App.calculateProbabilities2(new ArrayList<>()))
+                .isEqualTo(new HashMap<Integer, Map<Integer, Double>>());
+    }
+
+    @Test
+    void testCalculateProbabilities2_1() {
+        List<Integer> numbers = List.of(1, 3, 1, 5, 1);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(3, 0.5, 5, 0.5),
+                3, Map.of(1, 1.0),
+                5, Map.of(1, 1.0)
+        );
+
+        assertThat(App.calculateProbabilities2(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testCalculateProbabilities2_2() {
+        List<Integer> numbers = List.of(1, 3, 1, 5, 1, 2, 1, 6);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(2, 0.25, 3, 0.25, 5, 0.25, 6, 0.25),
+                2, Map.of(1, 1.0),
+                3, Map.of(1, 1.0),
+                5, Map.of(1, 1.0),
+                6, Map.of()
+        );
+
+        assertThat(App.calculateProbabilities2(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testCalculateProbabilities2_3() {
+        List<Integer> numbers = List.of(2, 3, 2, 5, 2, 2, 1, 3);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(3, 1.0),
+                2, Map.of(1, 0.25, 2, 0.25, 3, 0.25, 5, 0.25),
+                3, Map.of(2, 1.0),
+                5, Map.of(2, 1.0)
+        );
+
+        assertThat(App.calculateProbabilities2(numbers)).isEqualTo(expected);
+    }
+
+    @Test
+    void testCalculateProbabilities2_4() {
+        List<Integer> numbers = List.of(1, 3, 1, 5, 1, 2, 1, 6, 1, 3);
+        Map<Integer, Map<Integer, Double>> expected = Map.of(
+                1, Map.of(2, 0.2, 3, 0.4, 5, 0.2, 6, 0.2),
+                2, Map.of(1, 1.0),
+                3, Map.of(1, 1.0),
+                5, Map.of(1, 1.0),
+                6, Map.of(1, 1.0)
+        );
+
+        assertThat(App.calculateProbabilities2(numbers)).isEqualTo(expected);
     }
 }
