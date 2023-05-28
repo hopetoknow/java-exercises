2bd2b6626b92570c6414d3746f356232ea1c52a3
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-05-28 21:05:10 +0200

    Add second solution to horizontal-histogram exercise

--- exercises/collections/horizontal-histogram/src/main/java/exercise/App.java
+++ exercises/collections/horizontal-histogram/src/main/java/exercise/App.java
@@ -1,21 +1,21 @@
 package exercise;
 
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import java.util.function.Supplier;
 
 class App {
 
-    public static void play(int rollsNumber, Supplier<Integer> rollTheDice) {
-        List<String> histogram = Stream.of("1|", "2|", "3|", "4|", "5|", "6|").collect(Collectors.toList());
+    public static void play(int rollsNumber, Supplier<Integer> rollGenerator) {
+        List<String> histogramParts = Stream.of("1|", "2|", "3|", "4|", "5|", "6|").collect(Collectors.toList());
 
-        IntStream.range(0, rollsNumber)
+        IntStream.rangeClosed(1, rollsNumber)
             .forEach(roll -> {
-                int rollResult = rollTheDice.get();
+                int rollResult = rollGenerator.get();
                 int index = rollResult - 1;
-                String histogramPart = histogram.get(index);
+                String histogramPart = histogramParts.get(index);
                 String newHistogramPart;
 
                 if (histogramPart.endsWith("|")) {
@@ -30,10 +30,28 @@ class App {
                                 }
                             }).collect(Collectors.joining(" "));
                 }
-                histogram.set(index, newHistogramPart);
+                histogramParts.set(index, newHistogramPart);
             });
 
-        String histogramAsString = String.join("\n", histogram);
-        System.out.println(histogramAsString);
+        String histogram = String.join("\n", histogramParts);
+        System.out.println(histogram);
+    }
+
+    public static void play2(int rollsNumber, Supplier<Integer> rollGenerator) {
+        List<Integer> rollsResult = Stream.generate(rollGenerator)
+                .limit(rollsNumber)
+                .toList();
+
+        String histogram = IntStream.rangeClosed(1, 6)
+                .boxed()
+                .map(diceSide -> {
+                    long sideRollsNumber = rollsResult.stream().filter(number -> number == diceSide).count();
+                    String sideRollsNumberAsString = sideRollsNumber != 0 ? " " + sideRollsNumber : "";
+                    String hashPart = "#".repeat((int) sideRollsNumber);
+                    return String.format("%s|%s%s", diceSide, hashPart, sideRollsNumberAsString);
+                })
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(histogram);
     }
 }
