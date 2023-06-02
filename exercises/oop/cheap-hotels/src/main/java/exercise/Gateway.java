8332b6202e87b26112f5b35caed96806108a62b5
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-02 21:48:16 +0200

    Add second variant of findAll() method in Gateway class

--- exercises/oop/cheap-hotels/src/main/java/exercise/Gateway.java
+++ exercises/oop/cheap-hotels/src/main/java/exercise/Gateway.java
@@ -3,7 +3,9 @@ package exercise;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import exercise.strategy.BookingStrategy;
+import exercise.strategy.KayakStrategy;
 import exercise.strategy.OstrovokStrategy;
+import exercise.strategy.Strategy;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -26,7 +28,7 @@ public final class Gateway {
 
         for (Map<String, Object> serviceHotels : hotelsByService) {
             String service = serviceHotels.get("service").toString();
-            List<Map<String, Object>> hotels = (List<Map<String, Object>>) serviceHotels.get("hotels");
+            List<Map<String, Object>> hotels = (List) serviceHotels.get("hotels");
 
             for (Map<String, Object> hotel : hotels) {
                 Map<String, Object> hotelAndService = new HashMap<>();
@@ -39,7 +41,7 @@ public final class Gateway {
 
         return hotelInfos.stream()
                 .filter(hotelInfo -> {
-                    double cost = (double) ((Map<String, Object>)hotelInfo.get("hotel")).get("cost");
+                    double cost = (double) ((Map) hotelInfo.get("hotel")).get("cost");
                     Integer min = priceRangeFilters.get("min");
                     Integer max = priceRangeFilters.get("max");
 
@@ -56,6 +58,45 @@ public final class Gateway {
                 .collect(Collectors.toList());
     }
 
+    public List<Map<String, Object>> findAll2(Map<String, Integer> priceRangeFilters) {
+        Map<String, Strategy> strategies = Map.of(
+                "ostrovok", new OstrovokStrategy(),
+                "booking", new BookingStrategy(),
+                "kayak", new KayakStrategy()
+        );
+
+        Map<String, Integer> defaultPriceRangeFilters = Map.of(
+                "min", 0,
+                "max", Integer.MAX_VALUE
+        );
+
+        Map<String, Integer> mergedPriceRangeFilters = new HashMap<>(defaultPriceRangeFilters);
+        mergedPriceRangeFilters.putAll(priceRangeFilters);
+        int min = mergedPriceRangeFilters.get("min");
+        int max = mergedPriceRangeFilters.get("max");
+
+        return hotelsByService.stream().flatMap(serviceHotels -> {
+            String service = serviceHotels.get("service").toString();
+            List<Map<String, Object>> hotels = (List) serviceHotels.get("hotels");
+            Strategy strategy = strategies.get(service);
+
+            return hotels.stream()
+                    .map(hotel -> Map.of(
+                            "hotel", strategy.convert(hotel),
+                            "service", service
+                    ))
+                    .filter(hotelInfo -> {
+                       Map<String, Object> hotel = (Map) hotelInfo.get("hotel");
+                       double price = (double) hotel.get("cost");
+                       return price >= min && price <= max;
+                    });
+        }).toList();
+    }
+
+    public List<Map<String, Object>> findAll2() {
+        return findAll(new HashMap<String, Integer>());
+    }
+
     private static List<Map<String, Object>> getData(String fileName) {
         Path filePath = Paths.get("src", "main", "resources", fileName).toAbsolutePath().normalize();
         ObjectMapper mapper = new ObjectMapper();
@@ -67,13 +108,13 @@ public final class Gateway {
         }
     }
 
-     private Map<String, Object> convertPrice(String service, Map<String, Object> str) {
+     private Map<String, Object> convertPrice(String service, Map<String, Object> hotel) {
         if (service.equals("ostrovok")) {
-            return new OstrovokStrategy().convert(str);
+            return new OstrovokStrategy().convert(hotel);
         } else if (service.equals("booking")) {
-            return new BookingStrategy().convert(str);
+            return new BookingStrategy().convert(hotel);
         }
 
-        return str;
+        return hotel;
     }
 }
